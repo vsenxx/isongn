12b9f302fec9ef56caa4b3271db6c5401fdd7f92
uzudil <hunhm1@gmail.com>
2021-02-15 14:19:13 -0800

    grounds, edges

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -32,11 +32,18 @@ type Shape struct {
 	Fudge       float32
 	ImageIndex  int
 	ShapeMeta   *ShapeMeta
+	Flags       map[string]bool
+	EdgeOf      *Shape
 }
 
 var Shapes []*Shape
 var Names map[string]int = map[string]int{}
 var Images []image.Image
+
+var cornerSuffix [][]string = [][]string{
+	{"w", "e"},
+	{"n", "s"},
+}
 
 func InitShapes(gameDir string) error {
 	bytes, err := ioutil.ReadFile(filepath.Join(gameDir, "shapes.json"))
@@ -68,22 +75,38 @@ func InitShapes(gameDir string) error {
 		if err != nil {
 			return err
 		}
+		imageIndex := len(Images)
+		Images = append(Images, img)
 		for _, s := range shapes {
 			shapeDef := s.(map[string]interface{})
 			name := shapeDef["name"].(string)
 			fmt.Printf("\tProcessing %s\n", name)
 			Names[name] = len(Shapes)
-			Shapes = append(Shapes, initShape(name, shapeDef, len(Images), img, shapeMeta))
+			appendShape(name, shapeDef, imageIndex, img, shapeMeta)
 		}
-		Images = append(Images, img)
 	}
 	fmt.Printf("Loaded %d shapes.\n", len(Shapes))
 	return nil
 }
 
-func initShape(name string, shapeDef map[string]interface{}, imageIndex int, img image.Image, shapeMeta *ShapeMeta) *Shape {
+func appendShape(name string, shapeDef map[string]interface{}, imageIndex int, img image.Image, shapeMeta *ShapeMeta) {
+	// flags
+	flagsSet := map[string]bool{}
+	flags, ok := shapeDef["flags"]
+	if ok {
+		for _, flag := range flags.([]interface{}) {
+			flagsSet[flag.(string)] = true
+		}
+	}
+
+	// size
 	sizeI := shapeDef["size"].([]interface{})
 	size := [3]float32{float32(sizeI[0].(float64)), float32(sizeI[1].(float64)), float32(sizeI[2].(float64))}
+	if _, ok := flagsSet["stamp"]; ok {
+		size[2] = 0.01
+	}
+
+	// pixel bounding box
 	posI := shapeDef["pos"].([]interface{})
 	px := float32(posI[0].(float64)) * shapeMeta.DpiMultiplier
 	py := float32(posI[1].(float64)) * shapeMeta.DpiMultiplier
@@ -91,11 +114,75 @@ func initShape(name string, shapeDef map[string]interface{}, imageIndex int, img
 	unitPixelY := float32(shapeMeta.UnitPixels[1])
 	pw := (size[0] + size[1]) * unitPixelX * shapeMeta.DpiMultiplier
 	ph := (size[0] + size[1] + size[2]) * unitPixelY * shapeMeta.DpiMultiplier
+
+	// fudge
 	fudge64, ok := shapeDef["fudge"].(float64)
 	var fudge float32 = 0.01
 	if ok {
 		fudge = float32(fudge64)
 	}
+
+	// edge of...
+	var edgeOf *Shape
+	if edgeOfName, ok := shapeDef["edgeOf"].(string); ok {
+		for _, s := range Shapes {
+			if s.Name == edgeOfName {
+				edgeOf = s
+				break
+			}
+		}
+		if edgeOf == nil {
+			panic("Can't find edgeOf shape: " + edgeOfName)
+		}
+	}
+
+	if edgeOf != nil {
+		for xx := 0; xx < 2; xx++ {
+			for yy := 0; yy < 2; yy++ {
+
+				// make a new image for this corner piece
+				cw := int(pw / 2)
+				ch := int(ph / 2)
+				cx := int(px) + xx*cw
+				cy := int(py) + yy*ch
+				cornerImg := image.NewRGBA(image.Rect(0, 0, int(pw), int(ph)))
+				draw.Draw(cornerImg, image.Rect(xx*cw, yy*ch, xx*cw+cw, yy*ch+ch), img, image.Point{cx, cy}, draw.Src)
+
+				// and add it to Images
+				cornerImageIndex := len(Images)
+				Images = append(Images, cornerImg)
+
+				Shapes = append(Shapes, newShape(
+					len(Shapes),
+					name+"."+cornerSuffix[xx][yy],
+					size,
+					0, 0, pw, ph,
+					cornerImg,
+					fudge,
+					cornerImageIndex,
+					shapeMeta,
+					flagsSet,
+					edgeOf,
+				))
+			}
+		}
+	} else {
+		Shapes = append(Shapes, newShape(
+			len(Shapes),
+			name,
+			size,
+			px, py, pw, ph,
+			img,
+			fudge,
+			imageIndex,
+			shapeMeta,
+			flagsSet,
+			edgeOf,
+		))
+	}
+}
+
+func newShape(index int, name string, size [3]float32, px, py, pw, ph float32, img image.Image, fudge float32, imageIndex int, shapeMeta *ShapeMeta, flagsSet map[string]bool, edgeOf *Shape) *Shape {
 	imageBounds := img.Bounds()
 	shape := &Shape{
 		Index:       len(Shapes),
@@ -108,6 +195,8 @@ func initShape(name string, shapeDef map[string]interface{}, imageIndex int, img
 		Fudge:       fudge,
 		ImageIndex:  imageIndex,
 		ShapeMeta:   shapeMeta,
+		Flags:       flagsSet,
+		EdgeOf:      edgeOf,
 	}
 
 	// create a half-size thumbnail
@@ -118,6 +207,7 @@ func initShape(name string, shapeDef map[string]interface{}, imageIndex int, img
 	resized := resize.Resize(w, h, rgba, resize.NearestNeighbor)
 	shape.Image = image.NewRGBA(resized.Bounds())
 	draw.Draw(shape.Image, resized.Bounds(), resized, image.ZP, draw.Src)
+
 	return shape
 }
 
